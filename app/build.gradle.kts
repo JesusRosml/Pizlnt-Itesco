72bcfcebe25b877789fe08c1167909011e8eabe9
JesusRosml <jesusperezhidalgo16@outlook.com>
2024-06-26 18:49:00 -0600

    Autentificación de usuario finalizado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("com.android.volley:volley:1.2.1")
+    implementation("com.google.android.material:material:1.13.0-alpha03")
 }
--- app/src/main/java/com/example/pizint_itesco/FragmentLogin.kt
+++ app/src/main/java/com/example/pizint_itesco/FragmentLogin.kt
@@ -10,6 +10,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import com.example.pizint_itesco.databinding.FragmentFirstLoginBinding
+import android.util.Log
+import com.google.android.material.snackbar.Snackbar
+import android.content.Context
+import android.content.Intent
+import android.view.inputmethod.InputMethodManager
+
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
+
+import com.android.volley.NetworkResponse
+import com.android.volley.toolbox.HttpHeaderParser
+
 
 class FragmentLogin : Fragment() {
 
@@ -26,6 +41,18 @@ class FragmentLogin : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val buttonAuth = binding.buttonLogin
+        val textRegisterUser = binding.textViewRegister
+
+        buttonAuth.setOnClickListener {
+            authenticationUser()
+            closeKeyboard()
+        }
+
+        textRegisterUser.setOnClickListener {
+            openFragmentRegister()
+        }
+
         applySpannableText()
     }
 
@@ -33,16 +60,123 @@ class FragmentLogin : Fragment() {
         val fullText = "¿No tienes una cuenta? Regístrate"
         val spannableString = SpannableString(fullText)
 
-        // Aplica color a la parte "Regístrate"
         val startIndex = fullText.indexOf("Regístrate")
         val endIndex = startIndex + "Regístrate".length
-        // Usa ContextCompat para obtener el color entero
-        val colorInt = ContextCompat.getColor(requireContext(), R.color.colorTextRegister) // Asegúrate de tener un color definido en res/values/colors.xml
+        val colorInt = ContextCompat.getColor(
+            requireContext(),
+            R.color.colorTextRegister
+        )
         val colorSpan = ForegroundColorSpan(colorInt)
         spannableString.setSpan(colorSpan, startIndex, endIndex, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
 
-        // Aplica el SpannableString al TextView
         binding.textViewRegister.text = spannableString
+    }
+
+    class CustomJsonObjectRequest(
+        method: Int,
+        url: String,
+        requestBody: JSONObject?,
+        private val responseListener: Response.Listener<JSONObject>,
+        errorListener: Response.ErrorListener
+    ) : JsonObjectRequest(method, url, requestBody, responseListener, errorListener) {
+
+        override fun parseNetworkResponse(response: NetworkResponse): Response<JSONObject> {
+            return if (response.statusCode == 401) {
+                // Aquí manejas cómo quieres procesar los códigos 401.
+                // Por ejemplo, convertir la respuesta en un JSONObject.
+                val jsonResponse = JSONObject(String(response.data))
+                Response.success(jsonResponse, HttpHeaderParser.parseCacheHeaders(response))
+            } else {
+                // Para otros códigos, usa el comportamiento predeterminado.
+                super.parseNetworkResponse(response)
+            }
+        }
+    }
+
+    private fun authenticationUser() {
+        val emptyText = "^\\s*$".toRegex()
+        val email = binding.emailUser.text.toString()
+        val password = binding.passwordUser.text.toString()
+
+        if (emptyText.matches(email) || emptyText.matches(password)) {
+            Snackbar.make(binding.root, "Los campos de correo electrónico y contraseña no pueden estar vacíos", Snackbar.LENGTH_LONG).show()
+            return
+        }
+
+        val credentialUser = JSONObject()
+        try {
+            credentialUser.put("email", email)
+            credentialUser.put("password", password)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        val queue = Volley.newRequestQueue(context)
+        val url = "https://d641-187-188-26-224.ngrok-free.app/login"
+
+        val responseServer = CustomJsonObjectRequest(Request.Method.POST, url, credentialUser,
+            { response ->
+                val message = response.optString("message", "No se encontró nigun mensaje")
+                val auth = response.optBoolean("authentication", false )
+                val user = response.optJSONObject("user")
+
+                val id = user?.optString("_id", "No ID")
+                val name = user?.optString("name", "No Name")
+                val surname = user?.optString("surname", "No Surname")
+                val secondSurname = user?.optString("secondSurname", "No SecondSurname")
+                val email = user?.optString("email", "No Email")
+                val career = user?.optString("career", "No Career")
+                val isAdmin = user?.optBoolean("admin", false)
+                val typeUser = user?.optString("typeUser", "No TypeUser")
+
+                val sharedPreferences = activity?.getSharedPreferences("userData", Context.MODE_PRIVATE)
+                val editor = sharedPreferences?.edit()
+
+                editor?.putString("id", id)
+                editor?.putString("name", name)
+                editor?.putString("surname", surname)
+                editor?.putString("secondSurname", secondSurname)
+                editor?.putString("email", email)
+                editor?.putString("career", career)
+                if (isAdmin != null) { editor?.putBoolean("isAdmin", isAdmin) }
+                editor?.putString("typeUser", typeUser)
+
+                editor?.apply()
+
+                if( auth ) {
+                    val intent = Intent(activity, Home::class.java)
+                    activity?.startActivity(intent)
+                    activity?.finish()
+                }
+            },
+            { error ->
+                val responseBody = String(error.networkResponse.data, Charsets.UTF_8)
+
+                try {
+                    val jsonError = JSONObject(responseBody)
+                    val message = jsonError.optString("message", "No se encontró nigun mensaje")
+
+                    Snackbar.make(binding.root, message, Snackbar.LENGTH_LONG).show()
+                    Log.d("LoginFragment", "Error: $message")
+                } catch (e: Exception) {
+                    Log.d("LoginFragment", "Error: ${error.message}")
+                }
+            }
+        )
+
+        queue.add(responseServer)
+    }
+
+    private fun closeKeyboard() {
+        val view = view?.findFocus()
+        if (view != null) {
+            val imm = requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+    }
+
+    private fun openFragmentRegister() {
+        Log.d("LoginFragment", "Fragment de registro de usuario abierto")
     }
 
     override fun onDestroyView() {
